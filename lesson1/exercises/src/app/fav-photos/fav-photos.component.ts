0e02d270f68caf9f651cb4187d1b6a51b109e03a
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-07-26 23:49:55 -0500

    Starting part 2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Photos';
+  image1 = 'https://pixel.nymag.com/imgs/daily/vulture/2015/03/23/24-buscemi.o.jpg/a_190x190.w1200.h630.jpg';
+  image2 = 'https://content8.flixster.com/site/10/25/61/10256134_ori.jpg';
+  image3 = 'https://tvguide1.cbsistatic.com/mediabin/showcards/celebs/s-u/thumbs/steve-buscemi_sc_140x187.png';
 
   constructor() { }
 
